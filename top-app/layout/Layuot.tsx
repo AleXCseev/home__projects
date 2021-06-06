97bafc6d20dc96745b642940c411a789be95ee3d
Kontantin Alekseev <alexcseev1990@gmail.com>
2021-06-06 17:57:22 +0300

    add layout component

---
+++ top-app/layout/Layuot.tsx
@@ -0,0 +1,28 @@
+import { LayoutProps } from "./Layout.props";
+import styles from "./Layout.module.css";
+import cn from "classnames";
+import React from "react";
+import { Header } from "./Header/Header";
+import { Sidebar } from "./Sidebar/Sidebar";
+import { Footer } from "./Footer/Footer";
+
+export const Layout = ({ children, ...props }: LayoutProps): JSX.Element => {
+	return (
+		<>
+			<Header>
+
+			</Header>
+
+			<div>
+				<Sidebar></Sidebar>
+				<div>
+					{children}
+				</div>
+			</div>
+
+			<Footer>
+
+			</Footer>
+		</>
+	);
+};
--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Button, Tag, P, Raiting } from "../components";
+import { Layout } from "../layout/Layuot";
 
 export default function Home(): JSX.Element {
 	const [raiting, setRaiting] = useState<number>(4)
 	return (
-		<div>
+		<Layout>
 			<Button appearance={"primary"} arrow="down">
 				Button
 			</Button>
@@ -20,6 +21,6 @@ export default function Home(): JSX.Element {
 			<Tag size="m" color="grey">Ghost</Tag>
 			<Tag size="m" color="red">Ghost</Tag>
 			<Raiting raiting={raiting} isEditable setRaiting={setRaiting}></Raiting>
-		</div>
+		</Layout>
 	);
 }

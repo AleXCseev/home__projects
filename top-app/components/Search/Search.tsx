91fbeacec077359141e0501fdddb49c9391c3f7d
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-08-06 15:52:22 +0300

    fix

---
+++ top-app/components/Search/Search.tsx
@@ -0,0 +1,9 @@
+import { SearchProps } from "./Search.props";
+import styles from "./Search.module.css";
+import cn from "classnames";
+
+export const Search = ({  children, className, ...props }: SearchProps): JSX.Element => {
+	return (
+		
+	);
+};

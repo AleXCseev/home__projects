64623dcb6eb73a6603930aa438323296934ba727
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-09-13 14:06:18 +0300

    fix

--- top-api/src/review/dto/create-review.dto.ts
+++ top-api/src/review/dto/create-review.dto.ts
@@ -11,8 +11,8 @@ export class CreateReviewDto {
 	@IsString()
 	description: string;
 
-	@Max(5)
-	@Min(1)
+	@Max(5, { message: "Рейтинг не может быть больше 5"})
+	@Min(1, { message: "Рейтинг не может быть менее 1"})
 	@IsNumber()
 	rating: number;
 

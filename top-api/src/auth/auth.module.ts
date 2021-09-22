7d96b19fbb2679781650b5398d70b16b8317b475
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-09-22 14:20:22 +0300

    add userEmail decorator

--- top-api/src/auth/auth.module.ts
+++ top-api/src/auth/auth.module.ts
@@ -4,10 +4,10 @@ import { AuthController } from './auth.controller';
 import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { getJWTConfig } from 'src/configs/jwt.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';;
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { getJWTConfig } from 'src/configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
--- top-api/src/review/review.controller.ts
+++ top-api/src/review/review.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
@@ -14,6 +15,7 @@ export class ReviewController {
 		return this.reviewService.create(dto);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
 	async delete(@Param('id') id: string) {
 		const deletedDoc = await this.reviewService.delete(id);
@@ -24,7 +26,8 @@ export class ReviewController {
 
 	@UseGuards(JwtAuthGuard)
 	@Get('byProduct/:productId')
-	async getByProduct(@Param('productId') productId: string) {
-		return this.reviewService.findByProductId(productId.trim());
+	async getByProduct(@Param('productId') productId: string, @UserEmail() email: string) {
+		console.log(email)
+		return this.reviewService.findByProductId(productId);
 	}
 }

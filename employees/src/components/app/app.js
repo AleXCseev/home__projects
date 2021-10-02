ec238752d33e67aa0be69b62aae0605e18733de2
Konstantin Alekseev <alexcseev1990@gmail.com>
2021-10-02 19:15:12 +0300

    create employees pr

---
+++ employees/src/components/app/app.js
@@ -0,0 +1,13 @@
+import "./app.css";
+import { AppInfo } from "../app-info/app-info";
+
+export const App = () => {
+	return (
+		<div className="app">
+			<AppInfo></AppInfo>
+            <div className="search-panel">
+                
+            </div>
+		</div>
+	);
+};
---
+++ employees/src/components/search-panel/search-panel.js
@@ -0,0 +1,7 @@
+
+
+export const SearchPanel = () => {
+    return (
+        <input type="text" className="form-control search-input"  placeholder="Найти сотрудника" />
+    )
+}

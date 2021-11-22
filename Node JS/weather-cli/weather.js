e070da2fdfe237ada418f40f208ad435ddd57b39
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-11-22 16:49:34 +0200

    add api service

--- Node JS/weather-cli/weather.js
+++ Node JS/weather-cli/weather.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
 
 import { getArgs } from "./helpers/args.js"
-import { printHelp } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
+import { getWeather } from "./services/api.service.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
 
-const initCli = () => {
+const saveToken = async (token) => {
+    if(!token.length) {
+        printError("Не передан токен");
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
+        printSuccess("Токен сохранен")
+    } catch (e) {
+        printError(e.message);
+    }
+    
+}
+
+const initCli = async () => {
     const args = getArgs(process.argv)
 
     if(args.h) {
@@ -15,9 +30,10 @@ const initCli = () => {
         // safe sity
     }
     if(args.t) {
-        saveKeyValue("token", args.t);
+        return saveToken(args.t);
         // safe token
     }
+    console.log(await getWeather("London"));
     // return weather
 }
 

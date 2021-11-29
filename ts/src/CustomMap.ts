5189f48d350c12f318929f2954d0a43a4ffad6c1
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-11-29 13:23:02 +0200

    fix

--- ts/src/CustomMap.ts
+++ ts/src/CustomMap.ts
@@ -1,12 +1,18 @@
-import { User } from "./Users" 
-import { Company } from "./Company"
+export interface Mappable {
+	location: {
+		lat: number;
+		lng: number;
+	};
+	markerContent(): string;
+}
 
 export class CustomMap {
     private googleMap: google.maps.Map;
+	
 
     constructor(id: string) {
         this.googleMap = new google.maps.Map(document.getElementById(id), {
-			zoom: 1,
+			zoom: 3,
 			center: {
 				lat: 46.4775,
 				lng: 30.7326,
@@ -14,13 +20,20 @@ export class CustomMap {
 		});
     }
 
-	addMarker(mappable: User | Company): void {
-		new google.maps.Marker({
+	addMarker(mappable: Mappable): void {
+		const marker = new google.maps.Marker({
 			map: this.googleMap,
 			position: {
 				lat: mappable.location.lat,
 				lng: mappable.location.lng,
 			},
 		});
+
+		marker.addListener("click", () => {
+			const infoWindow = new google.maps.InfoWindow({
+				content: mappable.markerContent()
+			})
+			infoWindow.open(this.googleMap, marker);
+		})
 	}
 }

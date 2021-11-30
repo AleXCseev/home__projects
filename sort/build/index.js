054b947b58aa33e926c778a0009c0886a652c40d
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-11-30 17:15:13 +0200

    add sorter

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,1 +1,21 @@
-console.log("hello");
+"use strict";
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([-4, 3, 11, 5, -1]);
+sorter.sort();
+console.log(sorter.collection);

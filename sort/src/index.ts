909d563b3a5f46e0ad03fcba4ba879d0cdbf381a
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-12-07 17:22:38 +0200

    fix

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,30 +1,8 @@
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection"
 
-class Sorter {
-    constructor(public collection: number[] | string) {}
-
-    sort():void {
-        const { length } = this.collection
-
-        for( let i = 0; i < length; i++ ) {
-            for( let j = 0; j < length - i - 1; j++ ) {
-
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const temp = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = temp;
-                    }
-                }
-                
-                if (typeof this.collection === "string") {
-                    
-                }
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([-4, 3, 11, 5, -1]);
+const numbersCollection = new NumbersCollection([-4, 3, -300, 11, 5, -1])
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection)
+console.log(numbersCollection.data)
 

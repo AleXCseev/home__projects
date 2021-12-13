214035d9001f0b0eb7421ae6b657f06b229d1376
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-12-13 09:25:21 +0200

    finish sort

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,25 +1,19 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection"
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([-4, 3, 1000, -300, 11, 5, -1])
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data)
 
 const charactersCollection = new CharactersCollection("ZxYNBa");
-const sorterString = new Sorter(charactersCollection)
-sorterString.sort()
+charactersCollection.sort()
 console.log(charactersCollection.data);
-
 
 const linkedList = new LinkedList();
 linkedList.add(100);
 linkedList.add(-100);
 linkedList.add(8);
 linkedList.add(-12);
-
-const linkedSorter = new Sorter(linkedList)
-linkedSorter.sort();
+linkedList.sort();
 linkedList.print()

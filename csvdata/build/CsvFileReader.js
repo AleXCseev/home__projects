7c6461cfad4d35c8b2b9f802ad85a743da7ac776
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-12-16 15:34:36 +0200

    fix

--- csvdata/build/CsvFileReader.js
+++ csvdata/build/CsvFileReader.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CsvFileReader = void 0;
 const fs_1 = __importDefault(require("fs"));
+const util_1 = require("./util");
 class CsvFileReader {
     constructor(filename) {
         this.filename = filename;
@@ -13,8 +14,21 @@ class CsvFileReader {
     read() {
         this.data = fs_1.default.readFileSync(this.filename, {
             encoding: 'utf-8'
-        }).split("\n").map((item) => {
+        })
+            .split("\n")
+            .map((item) => {
             return item.split(",");
+        })
+            .map((item) => {
+            return [
+                (0, util_1.dateStringToDate)(item[0]),
+                item[1],
+                item[2],
+                parseInt(item[3]),
+                parseInt(item[4]),
+                item[5],
+                item[6]
+            ];
         });
     }
 }
--- csvdata/src/CsvFileReader.ts
+++ csvdata/src/CsvFileReader.ts
@@ -1,16 +1,33 @@
 import fs from "fs";
+import { dateStringToDate } from "./util";
+import { MatchResult } from "./MatchResult";
+
+type MatchData = [Date, string, string, number, number, MatchResult, string]
 
 export class CsvFileReader {
 
-    data: string[][] = [];
+    data: MatchData[] = [];
 
     constructor(public filename: string) {}
 
     read():void {
         this.data = fs.readFileSync(this.filename, {
             encoding: 'utf-8'
-        }).split("\n").map((item: string): string[] => {
+        })
+        .split("\n")
+        .map((item: string): string[] => {
             return item.split(",")
+        })
+        .map((item: string[]): MatchData => {
+            return [
+                dateStringToDate(item[0]),
+                item[1],
+                item[2],
+                parseInt(item[3]),
+                parseInt(item[4]),
+                item[5] as MatchResult,
+                item[6]
+            ]
         })
     }
 }

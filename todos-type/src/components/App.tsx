dcadabc9457a52394dfbfae56816e1e16f524f9f
Alekseev Konstantin <alexcseev1990@gmail.com>
2021-12-22 15:41:15 +0200

    fix

--- todos-type/src/components/App.tsx
+++ todos-type/src/components/App.tsx
@@ -1,8 +1,51 @@
 import React from "react";
+import { connect } from "react-redux"
+import { Todo, fetchTodos, deleteTodo } from "../actions"
+import { StoreState } from "../reducers"
 
-export class App extends React.Component {
+interface AppProps {
+    todos: Todo[];
+    fetchTodos: Function;
+    deleteTodo: typeof deleteTodo
+}
+
+class _App extends React.Component<AppProps> {
+
+    onButtonHandler = (): void => {
+        this.props.fetchTodos()
+    }
+
+    onTodoHandler = (id: number): void => {
+        this.props.deleteTodo(id);
+    }
+ 
+    renderTodos(): JSX.Element[] {
+        return this.props.todos.map((todo: Todo) => {
+            return (
+                <div onClick={() => this.onTodoHandler(todo.id)} key={todo.id}>{ todo.title }</div>
+            )
+        })
+    }
+
     render() {
-        return <div>Hello world!!!</div>
+        return (
+        <div>
+            <button onClick={this.onButtonHandler}>Fetch</button>
+            {this.renderTodos()}
+        </div>
+        )
     }
 }
+
+const mapStateToProps = ({ todos }: StoreState): {todos: Todo[]} => {
+    return {
+        todos
+    }
+}
+
+export const App = connect(
+    mapStateToProps,
+    { fetchTodos, deleteTodo }
+)(_App)
+
 
--- todos-type/src/reducers/index.ts
+++ todos-type/src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux"
+import { Todo } from "../actions";
+import { todosReducer } from "./todos"
 
-export const reducers = combineReducers({
-    counter: () => 0
+export interface StoreState {
+    todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
+    todos: todosReducer
 });

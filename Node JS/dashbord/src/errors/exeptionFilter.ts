b19b2607ec22ce62570a1db54dd5fd46400a9dfc
Kontantin Alekseev <alexcseev1990@gmail.com>
2022-08-16 13:49:33 +0300

    add inversify

--- Node JS/dashbord/src/errors/exeptionFilter.ts
+++ Node JS/dashbord/src/errors/exeptionFilter.ts
@@ -1,14 +1,18 @@
+import { TYPES } from './../types';
 import { HTTPError } from './http-error.class';
-import { LoggerService } from './../logger/logger.service';
 import { NextFunction, Request, Response } from 'express';
 import { IExeptionFilter } from './exeption.filter.interface';
+import { inject, injectable } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
+import 'reflect-metadata'
 
-
+@injectable()
 export class ExeptionFilter implements IExeptionFilter {
-    logger: LoggerService;
-    constructor(logger: LoggerService) {
+
+    constructor(@inject(TYPES.ILogger) private logger: ILogger) {
         this.logger = logger
     }
+
     catch(err: Error | HTTPError, req: Request, res: Response, next: NextFunction) {
         if(err instanceof HTTPError) {
             this.logger.error(`[${err.context}] Ошибка ${err.statusCode}: ${err.message}`);
--- Node JS/dashbord/src/users/users.controller.ts
+++ Node JS/dashbord/src/users/users.controller.ts
@@ -1,12 +1,18 @@
+import { IUserController } from './users.interface';
+import { TYPES } from './../types';
 import { HTTPError } from './../errors/http-error.class';
 import { NextFunction, Request, Response } from 'express';
 import { BaseController } from "../common/base.controller";
-import { LoggerService } from "../logger/logger.service";
+import { inject, injectable } from 'inversify';
+import { ILogger } from '../logger/logger.interface';
+import 'reflect-metadata'
 
-export class UserController extends BaseController {
+@injectable()
+export class UserController extends BaseController implements IUserController {
 
-    constructor(logger: LoggerService) {
-        super(logger);
+    constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+        super(loggerService);
+
         this.bindRoutes([
             {path: '/register', method: 'post', func: this.register},
             {path: '/login', method: 'post', func: this.login}
